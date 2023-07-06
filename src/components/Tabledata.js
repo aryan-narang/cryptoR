556909edbf00285c9d2db67f9c6908325ad6ec94
aryan-narang <aryannarang014@gmail.com>
2023-07-06 22:23:35 +0530

    border added in table

--- src/components/Tabledata.js
+++ src/components/Tabledata.js
@@ -21,15 +21,16 @@ export default function Tabledata(props) {
       <Link to={`/coin/${item.id}`}  style={{textDecoration:"none", color:"black"}}>
         <table  className="table table-borderless" style={{width: '76vw',margin: 'auto'}}>
         <tbody >
-       <tr id="tabledata"className='d-flex justify-content-evenly' style={{fontSize:'1.6rem',color:'white',padding:'20px 0px 20px 0px'}}>
+       <tr id="tabledata"className='d-flex justify-content-evenly' style={{fontSize:'1.6rem',color:'white',padding:'20px 0px 20px 0px', borderBottom:"3px solid grey"}}>
       <th className='TabD' scope="row" style={{width : '300px', textAlign : 'left'}}><div className='d-flex'><div><img src={item.image} alt="" style={{height : '40px', width : '40px',marginLeft:'10px',marginRight:'10px'}} /></div><div>{item.name}</div></div></th>
       <td className='TabD' style={{width : '300px',textAlign :'right' }}>{"$"+numberWithCommas(item.current_price.toFixed(2))}</td>
       <td className='TabD' style={{width : '300px', textAlign :'right',paddingRight:"27px" ,color:item.price_change_percentage_24h.toFixed(2)>0?'#0ecb81':'#FF0000'}}>{item.price_change_percentage_24h.toFixed(2)>0?'+'+item.price_change_percentage_24h.toFixed(2):item.price_change_percentage_24h.toFixed(2)}%</td>
       <td className='TabD' style={{width : '300px',textAlign :'right'}}>{"$"+numberWithCommas(item.market_cap)}</td>
   
     </tr>
+    {/* <hr style={{width : '83vw',padding:'0px', backgroundColor:'white',borderTop:'3px solid white' }} /> */}
   </tbody>
-  <hr style={{width : '83vw',padding:'0px', backgroundColor:'white',borderTop:'3px solid white' }} />
+  
 </table>
 
     </Link>
